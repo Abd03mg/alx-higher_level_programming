b40b3c4b7dd1cc8dab08a37f3939200939b02ae6
Abd03mg <eemgyg@gmail.com>
2023-11-30 03:49:03 -0800

    add

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first arg.
+        b: second arg.
+
+    Return:
+        the return vakue. a + b.
+    """
+    return (a + b)
+
+
+print('1 + 2 = {}'.format(add(1, 2)))

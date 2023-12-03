a84e092fec38738f47b7e241cf078925dfb2a952
Abdoemg <eemgyg@gmail.com>
2023-12-03 17:53:17 +0200

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import sys
-
-c = len(sys.argv) - 1
-print("{:d} {}".format(c, ("arument" if c == 1 else "arguments")), end='')
-print("{}".format("." if c == 0 else ":"))
-if c > 0:
-    for i in range(0, c):
-        print(i+1, ": {}".format(sys.argv[i+1]), sep='')
+if __name__ == "__main__":
+    import sys
+    c = len(sys.argv) - 1
+    print("{:d} {}".format(c, ("arument" if c == 1 else "arguments")), end='')
+    print("{}".format("." if c == 0 else ":"))
+    if c > 0:
+        for i in range(0, c):
+            print(i+1, ": {}".format(sys.argv[i+1]), sep='')

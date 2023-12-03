bf14de1b1497101a83ef38a64f8f3b894122fc93
Abdoemg <eemgyg@gmail.com>
2023-12-03 13:01:38 +0200

    import add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
+if __name__ == '__main__':
+    from add_0 import add
 
-def add(a, b):
-    """My addition function
+    a = 1
+    b = 2
 
-    Args:
-        a: first arg.
-        b: second arg.
-
-    Return:
-        the return vakue. a + b.
-    """
-    return (a + b)
-
-
-print('1 + 2 = {}'.format(add(1, 2)))
+    print('{:d} + {:d} = {:d}'.format(a, b, add(1, 2)))

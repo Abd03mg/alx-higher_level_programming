7ccac3ca4f95a613a182780ff8fb1c1615e57f11
Abdoemg <eemgyg@gmail.com>
2023-12-05 21:59:11 +0200

    err

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix == [[]]:
-        break
-    a = 0
-    counter = 0
-    for i in matrix:
+    if matrix != [[]]:
+        a = 0
         counter = 0
-        for k in i:
-            a = len(i)
-            if counter < a-1:
-                print("{:d} ".format(k), end="")
-            else:
-                print(k)
-            counter += 1
+        for i in matrix:
+            counter = 0
+            for k in i:
+                a = len(i)
+                if counter < a-1:
+                    print("{:d} ".format(k), end="")
+                else:
+                    print(k)
+                counter += 1

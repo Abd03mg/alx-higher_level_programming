571a251f5f8c02109086962b0d952ee7f7c702b6
Abdoemg <eemgyg@gmail.com>
2023-12-05 17:29:22 +0200

    no more c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,8 @@
 def no_c(my_string):
     new_string = ""
     for i in my_string:
-        if i not ("c" or "C"):
+        if i == ("c" or "C"):
+            pass
+        else:
             new_string += i
     return (new_string)

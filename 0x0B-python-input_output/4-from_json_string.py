be111016ce0cd7ecf15bd126a99b90d22d46515c
Abdoemg <eemgyg@gmail.com>
2024-01-30 04:32:45 +0200

    Object

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' From JSON string to object'''
+
+
+def to_json_string(my_str):
+    ''' function that convert from json to object.
+    Args:
+        my_object: input JSON string.
+    Return:
+        python object.
+    '''
+    load = __import__("json").loads
+    return (load(my_obj))

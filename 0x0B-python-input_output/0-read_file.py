1f0a4d3420df6cde6a33911da82ab64d74982f6c
Abdoemg <eemgyg@gmail.com>
2024-01-30 02:31:37 +0200

    reads

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+''' Module contains function that uses to read text file. '''
+
+
 def read_file(filename=""):
     ''' Function that reads file and prints it to stdout.
         Args:

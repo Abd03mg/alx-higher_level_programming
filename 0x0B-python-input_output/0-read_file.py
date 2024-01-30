7c54f595e8dc0f400db01cf2b2156e391fa3f9ce
Abdoemg <eemgyg@gmail.com>
2024-01-30 02:28:55 +0200

    reads

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/sur/bin/env python3
+def read_file(filename=""):
+    ''' Function that reads file and prints it to stdout.
+        Args:
+            filename: name of file to be read.
+    '''
+    with open(filename, "r", encoding="utf-8") as f:
+        for _ in f:
+            print("{:s}".format(_), end="")

83b678952a8035a58c869ea33089fe1b79389c0d
Abdoemg <eemgyg@gmail.com>
2024-03-14 03:20:35 +0200

    fact

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+function fact (a) {
+  if (a < 0) {
+    return -1;
+  }
+  if (a === 0 || isNaN(a)) {
+    return 1;
+  }
+  return (a * fact(a - 1));
+}
+
+console.log(fact(parseInt(process.argv[2])));

16addeec96fce75a8dee8b9c802e3a490294b88c
Abdoemg <eemgyg@gmail.com>
2024-03-14 03:58:30 +0200

    fds

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,6 +2,6 @@
 if (process.argv.length <= 3) {
   console.log('0');
 } else {
-  const lis = process.argv.slice(2).sort(function (a, b) { return b - a; }).reverse();
+  const lis = process.argv.slice(2).sort().reverse();
   console.log(lis[1]);
 }

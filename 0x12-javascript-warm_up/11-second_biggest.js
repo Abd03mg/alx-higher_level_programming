62ed34c9158372800570f85fac1ad9d76134b9e3
Abdoemg <eemgyg@gmail.com>
2024-03-14 03:53:26 +0200

    big

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if (process.argv.length === 2 || process.argv.length === 3) {
-  console.log(0);
+if (process.argv.length <= 3) {
+  console.log('0');
 } else {
   const lis = process.argv.slice(2, process.argv.length).sort().reverse();
   console.log(lis[1]);

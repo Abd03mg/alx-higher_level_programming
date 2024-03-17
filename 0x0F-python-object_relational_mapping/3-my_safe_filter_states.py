c63d7d2aec051be9b63900bbf70e08244eed64d2
abd03mg <eemgyg@gmail.com>
2024-03-17 07:19:11 +0200

    safe sql

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Script that lists all 'states' from database 'hbtn...'"""
+
+
+import MySQLdb
+from sys import argv as arg
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            host="localhost", user=arg[1], passwd=arg[2], db=arg[3])
+    cur = db.cursor()
+    if (';' not in arg[4]):
+        cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' \
+                ".format(arg[4]))
+        for i in cur.fetchall():
+            print(i)

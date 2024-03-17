ad2a6cfe89e2a2ede51fc77c32cddc831a4ccf21
abd03mg <eemgyg@gmail.com>
2024-03-17 06:20:46 +0200

    %N asc

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Script that lists all 'states' from database 'hbtn...'"""
+
+
+import MySQLdb
+from sys import argv as arg
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            host="localhost", user=arg[1], passwd=arg[2], db=arg[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' \
+            ORDER BY states.id ASC")
+    for i in cur.fetchall():
+        print(i)

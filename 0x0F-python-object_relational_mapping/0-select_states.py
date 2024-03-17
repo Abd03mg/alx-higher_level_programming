0b6e637c721b23456d220fa18c35661c8e61db7c
root <root@DESKTOP-9MDNB53.localdomain>
2024-03-17 05:27:41 +0200

    ds

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Script that lists all 'states' from database 'hbtn...'"""
+
+
+import MySQLdb
+from sys import argv as arg
+
+if __name__ == "__main__":
+    db = connect(
+            host="localhost", user=arg[1], passwd=arg[2], db=arg[3])
+    cur = db.cursor()
+    cur.excecute("SELECT * FROM htbn_0e_usa")
+    for i in cur.fetchall():
+        print(i)

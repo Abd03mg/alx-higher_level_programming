d44e318532abc15383be55990b45dffa1144962d
abd03mg <eemgyg@gmail.com>
2024-03-30 02:40:15 +0200

    err

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,5 +8,5 @@ req = request.Request("https://alx-intranet.hbtn.io/status")
 with request.urlopen(req) as res:
     con = res.read()
     print(
-        'Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8'
+        'Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 '
         'content: {}'.format(type(con), con, con.decode("utf-8")))

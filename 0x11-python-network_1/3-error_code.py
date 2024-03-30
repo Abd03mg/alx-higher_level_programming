15e89acadaf91deacc6d1380ba19c1b4f456a5ca
abd03mg <eemgyg@gmail.com>
2024-03-30 06:06:13 +0200

    HTTO code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" python script that sends request to URL 
+    and displays body. """
+
+from urllib import request
+from sys import argv
+
+if __name__ == "__main__":
+    req = request.Request(argv[1])
+    try:
+        with request.urlopen(req) as res:
+            print(res.read().decode('ascii'))
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))

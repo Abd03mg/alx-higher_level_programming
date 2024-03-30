a76cb6c5a705da430ebce67ea2f5c95e3c9ce38f
abd03mg <eemgyg@gmail.com>
2024-03-30 08:25:08 +0200

    API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" script that takes a letter as argument and
+sends a post request"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    q = argv[1] if len(argv) == 2 else ""
+    r = requests.post("http://0.0.0.0:5000/search_user", data={'q': q})
+    try:
+        js = eval(r.text)
+        if (not js):
+            print("No result")
+        else:
+            print("[{}] {}".format(js.get('id'), js.get('name')))
+    except Exception:
+        print("Not aa valid JSON")

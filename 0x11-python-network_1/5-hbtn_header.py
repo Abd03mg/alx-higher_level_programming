acef08485bea5504089b1f181d155b48d69449fd
abd03mg <eemgyg@gmail.com>
2024-03-30 06:18:55 +0200

    headers

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Python script that Uses
+'request' module to get X-Request-Id value."""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    r = requests.get(argv[1])
+    print(r.headers["X-Request-Id"])

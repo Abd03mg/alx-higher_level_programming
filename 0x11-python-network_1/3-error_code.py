c61dfaa39d88b220415fa337ab466a7a70e257d5
abd03mg <eemgyg@gmail.com>
2024-04-01 09:13:58 +0200

    das

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-""" python script that sends request to URL 
+""" python script that sends request to URL
     and displays body. """
 
-from urllib import request
+from urllib import request, parse
+import urllib.error
 from sys import argv
 
 if __name__ == "__main__":
     req = request.Request(argv[1])
+
     try:
         with request.urlopen(req) as res:
-            print(res.read().decode('ascii'))
-    except error.HTTPError as e:
+            print(res.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))

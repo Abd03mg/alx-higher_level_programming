e526c5d3653b022431cf454fe44601174345281e
abd03mg <eemgyg@gmail.com>
2024-04-01 19:27:59 +0200

    fds

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Script that uses REST API to get user ID."""
+
+if __name__ == '__main__':
+    from sys import argv
+    import requests
+
+
+    req = requests.get("https://api.github.com/user", auth=(argv[1], argv[2]))
+    print(req.json().get('id'))

84145d3f38fb61032450cac184ec3e24e46b54a6
abd03mg <eemgyg@gmail.com>
2024-04-01 23:55:39 +0200

    commit

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,13 +8,13 @@ if __name__ == '__main__':
     from sys import argv
 
     req = requests.get(
-            'https://api.github.com/repos/{}/{}/commits'\
+            'https://api.github.com/repos/{}/{}/commits'
             .format(argv[1], argv[2])).json()
 
     try:
         for x in range(0, 10):
-            print("{}: {}".format(req[x]\
-                .get('sha'), req[x].get('commit').get('author').get('name')))
+            print("{}: {}".format(req[x]
+                  .get('sha'), req[x].get('commit').get('author').get('name')))
 
     except IndexError:
-        print('',end='')
+        print('', end='')

6c950d40078f7e4fe058c812ad19eb631edc7294
abd03mg <eemgyg@gmail.com>
2024-04-01 19:35:11 +0200

    fds

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,6 +5,5 @@ if __name__ == '__main__':
     from sys import argv
     import requests
 
-
     req = requests.get("https://api.github.com/user", auth=(argv[1], argv[2]))
     print(req.json().get('id'))

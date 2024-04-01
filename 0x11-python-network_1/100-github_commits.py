e0e5b8b0b828e94d9f1bd440ab889a2b50678f2d
abd03mg <eemgyg@gmail.com>
2024-04-01 23:51:35 +0200

    commit

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Script that lists most recent 10 commits on
+Github repo"""
+
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    req = requests.get(
+            'https://api.github.com/repos/{}/{}/commits'\
+            .format(argv[1], argv[2])).json()
+
+    try:
+        for x in range(0, 10):
+            print("{}: {}".format(req[x]\
+                .get('sha'), req[x].get('commit').get('author').get('name')))
+
+    except IndexError:
+        pass
